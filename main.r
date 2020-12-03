a7a74662d7999b563d213efd256c6bef4e7f3074
mziegler611 <57671001+mziegler611@users.noreply.github.com>
2020-12-03 13:43:32 -0500

    Updated for Iris dataset

--- main.r
+++ main.r
@@ -5,6 +5,9 @@
 #--------------------------------------
 #install.packages("caret")
 #install.packages("caret", dependencies=c("Depends", "Suggests"))
+
+source("/Users/leianna/Documents/321F20/MLinR/functions.R", chdir = TRUE)
+#source("/Users/morganziegler/Desktop/MLinR-main/functions.R")
 	
 library(caret)
 library(readxl)
@@ -16,53 +19,31 @@ library(tidyselect)
 library(magrittr)
 library(readr)
 library(tidyverse)
-#--------------------------------------
-#Import table
-#myData <- read_excel("/Users/leianna/Documents/321F20/MLinR/catsvdogs.xlsx")
-myData <- read_excel("/Users/morganziegler/Desktop/MLinR-main/catsvdogs.xlsx")
-list_header<-names(myData)
 
-#--------------------------------------
-#print list of headers
-printHeader <-function(dataEnter){
-  list_header<-names(dataEnter)
-  for(val in 1:length(list_header)){
-    print(list_header[val])
-  }
-}
-#call the function
-printHeader(myData)
-#--------------------------------------
-#https://rstudio-conf-2020.github.io/r-for-excel/ggplot2.html
-
-#print out line graphs
-ggplot(data = myData, aes(x = `Dog Owning Households (1000s)`, y = `Cat Population`)) +
-  geom_line(color = "purple",linetype = "dashed")
-#print out point graphs, pch is the point shape
-ggplot(data = myData, aes(x = `Dog Owning Households (1000s)`, y = `Cat Population`)) +
-  geom_point(color = "blue",pch = 18,size = 4, alpha = 0.5)
-#print out rectangle graphs
-ggplot(data = myData, aes(x = `Dog Owning Households (1000s)`, y = `Cat Population`)) +
-  geom_col(aes(fill = `Cat Population`))
 
 
 data(iris)
 dataset <- iris
 
-
+#get 80% of the data to train the model on
 validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
 
 validation <- dataset[-validation_index,]
 
 dataset <- dataset[validation_index,]
-dim(dataset)
+
+
+print(dim(dataset))
 sapply(dataset, class)
-head(dataset)
+
+#print the first 5 rows of the data to see what it looks like
+print(head(dataset))
+
 levels(dataset$Species)
 percentage <- prop.table(table(dataset$Species)) * 100
-cbind(freq=table(dataset$Species), percentage=percentage)
+print(cbind(freq=table(dataset$Species), percentage=percentage))
 
-summary(dataset)
+print(summary(dataset))
 
 x <- dataset[,1:4]
 y <- dataset[,5]
@@ -71,10 +52,16 @@ y <- dataset[,5]
 
 par(mfrow=c(1,4))
 
+#plot the distribution of the features
 for(i in 1:4) {
   boxplot(x[,i], main=names(iris)[i])
 }
 
-#not runnin yet
-featurePlot(x=x, y=y, plot="ellipse")
+#plot the distribution of the species
+plot(y)
 
+
+#not runnin yet
+print(featurePlot(x, y, "ellipse"))
+print(featurePlot(x, y, "box"))
+print("done")
